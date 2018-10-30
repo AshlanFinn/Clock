7c8eb982f8052710bb236002fab7fd50b7c729d6
Koe-Nocturne <44598795+Koe-Nocturne@users.noreply.github.com>
2018-10-29 23:22:55 -0700

    Add files via upload

---
+++ HW3fxns.py
@@ -0,0 +1,134 @@
+import turtle
+import time
+import sys
+
+def turtle_init(color,bgcolor,pensize, speed, shape):
+    '''
+    Creates a screen and turtle object.
+    @param (color,bgcolor,pensize,speed,shape) -- input turtle characteristics
+    @return, (turtle,window) 
+    '''
+    window = turtle.Screen()
+    window.bgcolor(bgcolor)
+    turtle.mode('logo')
+    
+    terrapin = turtle.Turtle()
+    terrapin.ht()
+    terrapin.color(color)
+    terrapin.pensize(pensize)
+    terrapin.shape(shape)
+    terrapin.speed(speed)
+    terrapin.penup()
+    
+    return (terrapin,window)
+
+def draw_clock(terrapin,radius,pos_neg,message):
+    
+    terrapin.forward(radius)
+    terrapin.left(90)
+    terrapin.pendown()
+    terrapin.pensize(2)
+    terrapin.circle(radius,None,12)
+    terrapin.penup()
+    terrapin.home()
+
+    terrapin.forward(radius-100)
+    terrapin.left(90)
+    terrapin.pendown()
+    terrapin.circle(radius-100)
+    terrapin.penup()
+    terrapin.pensize(3)
+    terrapin.home()
+    
+    if pos_neg == '+':
+        numbers = range(1,13)
+    else:
+        numbers = range(-12,0)
+        message = message[::-1].swapcase()
+        
+    for num in numbers:
+        terrapin.right(num * 30)
+        terrapin.forward(radius-100)
+        terrapin.dot(10)
+        terrapin.forward(50)
+        terrapin.write(num,False,'center',('Courier',35,'normal'))
+        terrapin.home()
+
+    terrapin.forward(radius)
+    terrapin.write(message,False,'center',('Courier',30,'normal'))
+    terrapin.home()
+        
+    
+def clock_hands(terrapin, time, disp_hh, disp_mm, radius):
+    """
+    Draws hour and minute hands corresponding to a set of time parameters
+    @param 1: terrapin_, a turtle
+    @param 2: time_, list of time parameters
+    @param 3: radius, length of minute hand
+    @return none
+    """
+    time = list(time)
+    mm = (time[4] + disp_mm) % 60
+    hh = (time[3] + disp_hh + (time[4] + disp_mm)/60) % 12 
+    print( (hh,mm))
+    terrapin.pendown()
+    terrapin.right(hh * 30)
+    terrapin.forward(radius/2)
+    terrapin.stamp()
+    terrapin.home()
+    
+    terrapin.right(mm*6)
+    terrapin.forward(radius-5)
+    terrapin.stamp()
+    terrapin.home()
+    terrapin.penup()
+
+def validate_text(response, values, is_case_sensitive):
+    '''
+    Validate a text response
+    '''
+    if is_case_sensitive and response in values:
+        return True
+        
+    if not is_case_sensitive:
+        response = response.lower()
+        values = [x.lower() for x in values]
+        if response in values:
+            return True
+    return False
+
+
+def convert_validate_int(s):
+    '''
+    converts a string to a int if possible, and if not returns None:
+    @params s -- input string
+    @returns, int or None
+
+    '''
+    if len(s) >= 1 and s[0] == '-':
+        if s[1:].isdigit():
+            return int(s)
+    elif s.isdigit():
+        return int(s)
+    else:
+        return None #s does not represent any kind of int
+
+
+
+
+##DEBUG##
+
+#DEBUG = True
+#while DEBUG:
+#    (ruth,canvas) = turtle_init("purple", "white", 3,0, "classic" )
+# 
+#    joe = ruth.clone()
+ #   time_params = time.localtime()
+#    disp = 0
+ #   radius = 200
+#    clock_hands(ruth, time_params, 0,0, radius  )
+#    draw_clock(joe,radius+100,'+','Hello World')
+ #   break
+
+
+    
---
+++ Homework_3_grwk.py
@@ -0,0 +1,193 @@
+###n our honor, as CCSF students, we Ashlan Finn and Jennifer Garcia have neither given or received inappropriate help with this assignment.
+###
+
+#Step 1:We will import all of the background information we need for our program to function
+#Step2:Create variables which will be called throughout the program
+#Step3:Make loop function that keeps track of how many times the user has played \
+#Step3pt2:and taliors response accordingly
+#Step4:Call functions from HWfxns to use in the loop
+
+import turtle
+import time
+import sys
+import HW3fxns
+#variables which we will call later in the program
+PosNum = ("12","1","2","3","4","5","6","7","8","9","10","11") #numbers that will be printed out 
+NegNum = ("-12","-11","-10","-9","-8","-7","-6","-5","-4","-3","-2","-1") #numbers that will be printed out
+time_params = list(time.localtime())
+NumTr = 0
+radius = 200
+'''
+Chat text which will be called throughout the program
+'''
+ChaText = ["Hello and welcome to our program  that uses clocks Yay! Do you want to take part in our program? Y or N. ",\
+           "Once more [Y/N]", "Do you see your world as negative or positive [-/+]? ","How do you rate your world this time, plus or minus [+/-]? ",\
+           "The time is now. Your reaction to that bracing thought? ", "The time is still now. Your reaction? ", "How will you change the time? in HH:MM format "]  
+
+
+while True:
+
+    #If this is the first time playing responses will be taliored
+    if NumTr == 0:
+        userinput = input(ChaText[0])
+        #validating inputs
+        Error = HW3fxns.validate_text(userinput, "YN", False)
+        if Error == False:
+            print("Your answer was not a valid response")
+            continue
+    else:
+        userinput = input(ChaText[1])
+        Error = HW3fxns.validate_text(userinput, "YN", False)
+        if Error == False:
+            print("Your answer was not a valid response")
+            continue
+    if userinput == "N":
+        raise SystemExit(0)
+    else:
+        NumTr += 1
+    #using 1 to determine if this is the first playthrough to determine text   
+    if NumTr <= 1:    
+        PNans = input(ChaText[2])
+        Error = HW3fxns.validate_text(PNans, "+/-", False)
+        #Validating the + or -
+        if Error == False:
+            print("Your answer was not a valid response")
+            continue
+        #Positive clock first time around
+        if PNans == '+':
+            (joe,canvas) = HW3fxns.turtle_init("black", "red", 3, 0, "classic" )
+            ruth = joe.clone()
+            respon = input(ChaText[4])
+            HW3fxns.draw_clock(joe, radius+100, '+' , respon)
+            HW3fxns.clock_hands(ruth, time_params, 0,0, radius  )            
+            HHMM = input(ChaText[6]).split(':')
+            if len(HHMM) != 2:
+                print(str(HHMM) + " is not two ints seperated by ':' please try again")
+                continue
+            NewHours = HW3fxns.convert_validate_int(HHMM[0])
+            NewMinutes = HW3fxns.convert_validate_int(HHMM[1])
+            if NewHours == None or NewMinutes == None:
+                print(test + ' is not a valid response, try again')
+                continue
+            (joe,canvas) = HW3fxns.turtle_init("red", "black", 3, 0, "classic" )
+            HW3fxns.draw_clock(joe, radius+100, '+' , respon)
+            HW3fxns.clock_hands(joe, time_params, NewHours,NewMinutes, radius  )
+
+            continue
+        
+        #negative clock first time around
+        elif PNans == '-':
+            (joe,canvas) = HW3fxns.turtle_init("green", "Black", 3, 0, "classic" )
+            ruth = joe.clone()
+            respon = input(ChaText[4])
+            HW3fxns.draw_clock(joe,radius+ 100, '-' , respon)
+            HW3fxns.clock_hands(ruth, time_params, 0,0, radius  )
+            HHMM = input(ChaText[6]).split(':')
+            if len(HHMM) != 2:
+                print(str(HHMM) + " is not two ints seperated by ':' please try again")
+                continue
+            NewHours = HW3fxns.convert_validate_int(HHMM[0])
+            NewMinutes = HW3fxns.convert_validate_int(HHMM[1])
+            if NewHours == None or NewMinutes == None:
+                print(NewHours+" " NewMinutes + ' is not a valid response, try again')
+                continue
+            (joe,canvas) = HW3fxns.turtle_init("green", "black", 3, 0, "classic" )
+            HW3fxns.draw_clock(joe,radius+ 100, '-' , respon)
+            HW3fxns.clock_hands(joe, time_params, NewHours,NewMinutes, radius  )
+            continue
+    #If this isn't the first time playing then different text appears
+    else:
+        PNans = input(ChaText[3])
+        Error = HW3fxns.validate_text(PNans, "+/-", False)
+        #Validating the + or -
+        if Error == False:
+            print("Your answer was not a valid response")
+            continue
+        #positive clock second or more times around
+        if PNans == '+':
+            (joe,canvas) = HW3fxns.turtle_init("green", "Black", 3, 0, "classic" )
+            ruth = joe.clone()
+            respon = input(ChaText[5])
+            HW3fxns.draw_clock(joe,radius+ 100, '+' , respon)
+            HW3fxns.clock_hands(ruth, time_params, 0,0, radius  )
+            HHMM = input(ChaText[6]).split(':')
+            if len(HHMM) != 2:
+                print(str(HHMM) + " is not two ints seperated by ':' please try again")
+                continue
+            NewHours = HW3fxns.convert_validate_int(HHMM[0])
+            NewMinutes = HW3fxns.convert_validate_int(HHMM[1])
+            if NewHours == None or NewMinutes == None:
+                print(NewHours+" " NewMinutes + ' is not a valid response, try again')
+                continue
+            (joe,canvas) = HW3fxns.turtle_init("green", "black", 3, 0, "classic" )
+            HW3fxns.draw_clock(joe,radius+ 100, '+' , respon)
+            HW3fxns.clock_hands(joe, time_params, NewHours,NewMinutes, radius  )
+            continue
+
+        #negative clock second or more times around
+        elif PNans == '-':
+            (joe,canvas) = HW3fxns.turtle_init("green", "Black", 3, 0, "classic" )
+            ruth = joe.clone()
+            respon = input(ChaText[5])
+            HW3fxns.draw_clock(joe,radius+ 100, '-' , respon)
+            HW3fxns.clock_hands(ruth, time_params, 0,0, radius  )
+            HHMM = input(ChaText[6]).split(':')
+            if len(HHMM) != 2:
+                print(str(HHMM) + " is not two ints seperated by ':' please try again")
+                continue
+            NewHours = HW3fxns.convert_validate_int(HHMM[0])
+            NewMinutes = HW3fxns.convert_validate_int(HHMM[1])
+            if NewHours == None or NewMinutes == None:
+                print(NewHours+" " NewMinutes + ' is not a valid response, try again')
+                continue
+            (joe,canvas) = HW3fxns.turtle_init("green", "black", 3, 0, "classic" )
+            HW3fxns.draw_clock(joe,radius+ 100, '-' , respon)
+            HW3fxns.clock_hands(joe, time_params, NewHours,NewMinutes, radius  )
+            continue
+    '''
+    '''
+
+
+### Test Reports
+
+###Test 1:
+#===================== RESTART: D:\HW3\Homework_3_grwk.py =====================
+#Hello and welcome to our program  that uses clocks Yay! Do you want to take part in our program? Y or N. Y
+#How do you rate your world this time, plus or minus [+/-]? +
+#The time is still now. Your reaction? asdf
+#(10.883333333333333, 53)
+#Traceback (most recent call last):
+#  File "D:\HW3\Homework_3_grwk.py", line 113, in <module>
+#    HHMM = input(ChaText[7]).split(':')
+#IndexError: list index out of range
+#>>>
+
+### Comments: Minor fixing with signs
+
+
+
+###Test 2:
+#===================== RESTART: D:\HW3\Homework_3_grwk.py =====================
+#Hello and welcome to our program  that uses clocks Yay! Do you want to take part in our program? Y or N. N
+#>>>
+
+###Comments:Works as planned
+
+
+###Test 3:
+#===================== RESTART: D:\HW3\Homework_3_grwk.py =====================
+#Hello and welcome to our program  that uses clocks Yay! Do you want to take part in our program? Y or N. Y
+#Do you see your world as negative or positive [-/+]? +
+#The time is now. Your reaction to that bracing thought? Sfds
+(#10.916666666666668, 55)
+#How will you change the time? in HH:MM format 5:40
+#(4.583333333333332, 35)
+#Once more [Y/N]Y
+#How do you rate your world this time, plus or minus [+/-]? -
+#The time is still now. Your reaction? SFddf
+#(10.916666666666668, 55)
+#Traceback (most recent call last):
+#  File "D:\HW3\Homework_3_grwk.py", line 134, in <module>
+#    HHMM = list(input(ChaText[7]).split(':'))
+#IndexError: list index out of range
+#>>>
